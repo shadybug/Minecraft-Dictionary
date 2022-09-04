32c1ddbea574e6e7ad56cda7ee709ce7b45225ab
Cicada Carpenter <cicada.carpenter@gmail.com>
2022-09-04 17:15:23 -0600

    Added home page

--- app.py
+++ app.py
@@ -3,16 +3,17 @@ import flask
 app = flask.Flask(__name__)
 
 dictionary = {
-    "Leopard Gecko" : "A desert-dwelling lizard with dark spots. Unlike most geckos, they have eyelids.",
-    "Ball Python" : 'A snake species common for beginner pet owners. Known for their tendency to "ball up" when nervous, which gives them their name.',
+    "LeopardGecko" : "A desert-dwelling lizard with dark spots. Unlike most geckos, they have eyelids.",
+    "BallPython" : 'A snake species common for beginner pet owners. Known for their tendency to "ball up" when nervous, which gives them their name.',
     "Hognose Snake" : "A burrowing snake species, that uses its pointed snout like a shovel. They play dead when threatened.",
     "Iguana" : "A bright green lizard that lives in tropical environments. Difficult to keep in captivity, as they grow very large and need a lot of space.",
-    "Red-eared Slider" : "A turtle species that lives in ponds and lakes. They were named for the red stripes on the side of their head, and are the most commonly kept species of turtle.",
+    "Red-earedSlider" : "A turtle species that lives in ponds and lakes. They were named for the red stripes on the side of their head, and are the most commonly kept species of turtle.",
+    "Test" : "This is a test!",
 }
 
 @app.route('/')
 def home():
-    return flask.render_template('index.html')
+    return flask.render_template('main.html', entries = dictionary)
 
 @app.route('/def/<item>')
 def lookup(item):
