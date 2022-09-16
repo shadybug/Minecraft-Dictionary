3fc724d9fb86fd74830a75e201726d1b1e461fc7
Cicada Carpenter <cicada.carpenter@gmail.com>
2022-09-16 17:26:32 -0600

    Updated with templates

--- app.py
+++ app.py
@@ -5,9 +5,18 @@ app = flask.Flask(__name__)
 dictionary = {
     "LeopardGecko" : "A desert-dwelling lizard with dark spots. Unlike most geckos, they have eyelids.",
     "BallPython" : 'A snake species common for beginner pet owners. Known for their tendency to "ball up" when nervous, which gives them their name.',
-    "Hognose Snake" : "A burrowing snake species, that uses its pointed snout like a shovel. They play dead when threatened.",
-    "Iguana" : "A bright green lizard that lives in tropical environments. Difficult to keep in captivity, as they grow very large and need a lot of space.",
+    "HognoseSnake" : "A burrowing snake species, that uses its pointed snout like a shovel. They play dead when threatened.",
+    "Iguana" : "A spiny lizard that lives in tropical environments. Difficult to keep in captivity, as they grow very large and need a lot of space.",
     "Red-earedSlider" : "A turtle species that lives in ponds and lakes. They were named for the red stripes on the side of their head, and are the most commonly kept species of turtle.",
+    "Test" : "This is a test!",
+}
+
+images = {
+    "LeopardGecko" : "https://upload.wikimedia.org/wikipedia/commons/5/5b/Eublepharis_macularius1.jpg",
+    "BallPython" : "https://upload.wikimedia.org/wikipedia/commons/4/4d/Ball_python_lucy.JPG",
+    "HognoseSnake" : "https://upload.wikimedia.org/wikipedia/commons/7/76/Plains_Hognose_Snake_(Heterodon_nasicus)_(29833441881).jpg",
+    "Iguana" : "https://upload.wikimedia.org/wikipedia/commons/e/ec/Iguana_iguana_Portoviejo_01.jpg",
+    "Red-earedSlider" : "https://upload.wikimedia.org/wikipedia/commons/e/eb/Trachemys_scripta_elegans_side.jpg",
     "Test" : "This is a test!",
 }
 
@@ -17,4 +26,4 @@ def home():
 
 @app.route('/def/<item>')
 def lookup(item):
-    return flask.render_template('index.html', item = item, definition = dictionary[item])
+    return flask.render_template('definition.html', item = item, definition = dictionary[item], image = images[item])
